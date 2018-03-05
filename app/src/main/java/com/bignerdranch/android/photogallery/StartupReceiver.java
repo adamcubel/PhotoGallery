3564c682a80e89c285bcf7b8d02747ea1c0bcdf4
adamcubel <adamcubel@yahoo.com>
2018-03-04 18:34:27 -0600

    29.5

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by adamc on 3/4/2018.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
