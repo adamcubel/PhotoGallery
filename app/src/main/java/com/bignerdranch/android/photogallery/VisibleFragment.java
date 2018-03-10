13bb1fc8fbb36dbc49fb75af29ece3971b55d399
adamcubel <adamcubel@yahoo.com>
2018-03-10 07:55:14 -0600

    Chapter 29 Completion

--- app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package com.bignerdranch.android.photogallery;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -18,9 +20,10 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast: " + intent.getAction(),
-                    Toast.LENGTH_LONG).show();
+            // if we receive this, we're visible, so cancel the notification
+            Log.i(TAG, "canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
+
         }
     };
 
@@ -28,7 +31,7 @@ public abstract class VisibleFragment extends Fragment {
     public void onStart() {
         super.onStart();
         IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
-        getActivity().registerReceiver(mOnShowNotification, filter);
+        getActivity().registerReceiver(mOnShowNotification, filter, PollService.PERM_PRIVATE, null);
     }
 
     @Override
