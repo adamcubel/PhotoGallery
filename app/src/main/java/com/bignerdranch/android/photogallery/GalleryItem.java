033bc0e461d889958e1b5a31553f8bff29fa62fd
adamcubel <adamcubel@yahoo.com>
2018-03-12 20:43:00 -0500

    Chapter 30 first half

--- app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by adamc on 2/3/2018.
  */
@@ -9,6 +11,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     @Override
     public String toString() {
@@ -37,5 +40,21 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("https://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 }
